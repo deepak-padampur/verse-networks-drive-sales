90c18cca0c777a4397d6613fbb01e540ef0c683d
deepak-padampur <deepaksuna4290@gmail.com>
2020-04-04 11:31:21 +0530

    Revert "dashboard layout fixed"
    This reverts commit fb0bcbd4bc4bcf413e520d9219d2013f1e30d572.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import NavbarLayout from './components/UI/Layout/Navbar';
-import Dashboard from './components/dashboard/Dashboard';
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <NavbarLayout />
-        <Switch>
-          <Route path="/" component={Dashboard} />
-        </Switch>
       </div>
 
     </BrowserRouter>
